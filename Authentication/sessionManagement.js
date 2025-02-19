86c8bffc06a53d666f52e9e3ebb2e66fd0dd1777
Divyanshi <85859444+Divyanshi2408@users.noreply.github.com>
2025-02-19 23:39:56 +0530

    add files

---
+++ Authentication/sessionManagement.js
@@ -0,0 +1,12 @@
+const session = require("express-session");
+
+exports.sessionMiddleware = session({
+  secret: process.env.SESSION_SECRET || "supersecretkey",
+  resave: false,
+  saveUninitialized: false,
+  cookie: {
+    secure: process.env.NODE_ENV === "production",
+    httpOnly: true,
+    maxAge: 24 * 60 * 60 * 1000, // 1 day
+  },
+});
